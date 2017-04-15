972253c8c44546b412c4be1d8ef90eb8d2abcd99
Pol Vilà <polvilasalo@gmail.com>
2017-04-15 22:40:01 +0200

    Add CD and PC meshes/animations

---
+++ Source/VoidSpace/CdAnimInstance.cpp
@@ -0,0 +1,11 @@
+// All rights Neebula Games
+
+#include "VoidSpace.h"
+#include "CdAnimInstance.h"
+
+UCdAnimInstance::UCdAnimInstance(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+
---
+++ Source/VoidSpace/CdAnimInstance.h
@@ -0,0 +1,23 @@
+// All rights Neebula Games
+
+#pragma once
+
+#include "CdAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS(transient, Blueprintable, hideCategories = AnimInstance, BlueprintType)
+class VOIDSPACE_API UCdAnimInstance : public UAnimInstance
+{
+	GENERATED_UCLASS_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = CdStates)
+	bool bIsInserting;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = CdStates)
+	bool bIsInserted;
+	
+	
+};
