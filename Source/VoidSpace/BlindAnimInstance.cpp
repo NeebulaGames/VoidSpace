83446ddfba67b4b8ffa82107c9ab3e0e66b895ed
Ennio Casas Puglielli <theepsi@gmail.com>
2017-04-17 21:00:52 +0200

    Add Blind's State Machine

---
+++ Source/VoidSpace/BlindAnimInstance.cpp
@@ -0,0 +1,10 @@
+// All rights Neebula Games
+
+#include "VoidSpace.h"
+#include "BlindAnimInstance.h"
+
+
+UBlindAnimInstance::UBlindAnimInstance(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer), bIsClosed(true)
+{
+}
---
+++ Source/VoidSpace/BlindAnimInstance.h
@@ -0,0 +1,29 @@
+// All rights Neebula Games
+
+#pragma once
+
+#include "BlindAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS(transient, Blueprintable, hideCategories = AnimInstance, BlueprintType)
+class VOIDSPACE_API UBlindAnimInstance : public UAnimInstance
+{
+	GENERATED_UCLASS_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = BlindStates)
+	bool bIsOpening;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = BlindStates)
+	bool bIsClosing;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = BlindStates)
+	bool bIsOpened;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = BlindStates)
+	bool bIsClosed;
+	
+	
+};
