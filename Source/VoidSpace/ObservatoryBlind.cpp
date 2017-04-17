8b974dcf6793b83bfcae29d5f5751fafb735beb9
Ennio Casas Puglielli <theepsi@gmail.com>
2017-04-17 21:02:58 +0200

    Add Blind Actor

---
+++ Source/VoidSpace/ObservatoryBlind.cpp
@@ -0,0 +1,31 @@
+// All rights Neebula Games
+
+#include "VoidSpace.h"
+#include "ObservatoryBlind.h"
+
+
+// Sets default values
+AObservatoryBlind::AObservatoryBlind()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	USceneComponent* root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = root;
+
+	static ConstructorHelpers::FObjectFinder<UClass> blindBlueprint(TEXT("Class'/Game/Animations/BlindBlueprint.BlindBlueprint_C'"));
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> blinds(TEXT("SkeletalMesh'/Game/Meshes/Blinds/Blinds_Animated.Blinds_Animated'"));
+	BlindMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Blinds"));
+	BlindMeshComponent->SetupAttachment(RootComponent);
+	BlindMeshComponent->SetSkeletalMesh(blinds.Object);
+	BlindMeshComponent->SetAnimInstanceClass(blindBlueprint.Object);
+
+}
+
+// Called when the game starts or when spawned
+void AObservatoryBlind::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/VoidSpace/ObservatoryBlind.h
@@ -0,0 +1,26 @@
+// All rights Neebula Games
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "ObservatoryBlind.generated.h"
+
+UCLASS()
+class VOIDSPACE_API AObservatoryBlind : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AObservatoryBlind();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+
+	UPROPERTY(VisibleAnywhere, Category = BlindMesh, meta = (AllowPrivateAccess = "true"))
+	class USkeletalMeshComponent* BlindMeshComponent;
+	
+};
