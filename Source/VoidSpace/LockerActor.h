6169aaeb92b61ee5ebbb959b58e34094da2bdb5f
Joel Lopez <jowie.jowie@gmail.com>
2017-05-26 23:02:09 +0200

    Add locker

---
+++ Source/VoidSpace/LockerActor.h
@@ -0,0 +1,32 @@
+// All rights Neebula Games
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "LockerActor.generated.h"
+
+UCLASS()
+class VOIDSPACE_API ALockerActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALockerActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, Category = Interactable, meta = (AllowPrivateAccess = "true"))
+	class UInteractableComponent* InteractableComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = Interactable, meta = (AllowPrivateAccess = "true"))
+	class USkeletalMeshComponent* LockerMeshComponent;
+
+	UFUNCTION()
+	void OnLockerUse();
+	
+};
