ed7171218bd1c928fa6bd411a90eb1f437829b87
Ennio Casas Puglielli <theepsi@gmail.com>
2017-05-28 18:36:28 +0200

    Adjust picking distance

--- Source/VoidSpace/SpaceCharacter.cpp
+++ Source/VoidSpace/SpaceCharacter.cpp
@@ -44,7 +44,7 @@ void ASpaceCharacter::Tick(float DeltaTime)
 	{
 		const FVector Start = UGameplayStatics::GetPlayerCameraManager(GetWorld(), 0)->GetCameraLocation();
 		const FVector dir_camera = UGameplayStatics::GetPlayerCameraManager(GetWorld(), 0)->GetActorForwardVector();
-		const FVector End = Start + dir_camera * 75;
+		const FVector End = Start + dir_camera * 150;
 
 		APawn* pawn = UGameplayStatics::GetPlayerController(GetWorld(), 0)->GetPawn();
 
