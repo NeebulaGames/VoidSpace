ff7fddbd0bf0c7a68a253621bf31cd0d44edce57
Joel Lopez <jowie.jowie@gmail.com>
2017-05-28 20:24:11 +0200

    Add dialogue trigger

---
+++ Source/VoidSpace/DialogueTriggerComponent.h
@@ -0,0 +1,32 @@
+// All rights Neebula Games
+
+#pragma once
+
+#include "Components/SceneComponent.h"
+#include "DialogueTriggerComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class VOIDSPACE_API UDialogueTriggerComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDialogueTriggerComponent();
+
+	UPROPERTY(EditAnywhere, Category = Dialogue)
+	FString DialogueName;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+
+	class UInteractableComponent* InteractableComponent;
+
+	UFUNCTION()
+	void OnTriggerEnter();
+	
+};
