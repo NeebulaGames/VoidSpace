7ebc09ff72dd0398ea4a813e9ecfa4cc1e49f462
polvila <polvilasalo@gmail.com>
2017-07-16 13:58:49 +0200

    Add Meteorites to the Map

---
+++ Source/ProjectMILA/MeteoritesActor.cpp
@@ -0,0 +1,38 @@
+// All rights Neebula Games
+
+#include "ProjectMILA.h"
+#include "MeteoritesActor.h"
+#include "SpaceGameStateBase.h"
+#include "GameEventManager.h"
+
+
+// Sets default values
+AMeteoritesActor::AMeteoritesActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> particles(TEXT("ParticleSystem'/Game/Particles/P_Ribbon_Meteorites.P_Ribbon_Meteorites'"));
+	ParticlesComponent = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("Meteorites"));
+	ParticlesComponent->SetTemplate(particles.Object);
+
+	RootComponent = ParticlesComponent;
+
+}
+
+// Called when the game starts or when spawned
+void AMeteoritesActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	ASpaceGameStateBase::Instance(GetWorld())->GameEventManager->OnEventFinished.AddDynamic(this, &AMeteoritesActor::StopEmission);
+}
+
+void AMeteoritesActor::StopEmission()
+{
+	if (ASpaceGameStateBase::Instance(GetWorld())->GameEventManager->GetCurrentEvent()->Name.Equals(FString("The Meteor")))
+		ParticlesComponent->bSuppressSpawning = true;
+}
+
+
+
---
+++ Source/ProjectMILA/MeteoritesActor.h
@@ -0,0 +1,30 @@
+// All rights Neebula Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MeteoritesActor.generated.h"
+
+UCLASS()
+class PROJECTMILA_API AMeteoritesActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMeteoritesActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+
+private:	
+	UPROPERTY(EditAnywhere)
+	class UParticleSystemComponent* ParticlesComponent = nullptr;
+	
+	UFUNCTION()
+	void StopEmission();
+	
+};
