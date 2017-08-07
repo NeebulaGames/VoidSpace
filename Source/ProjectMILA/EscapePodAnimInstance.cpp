f737198d20ea34bee95f07d406a23f33379c65fb
polvila <polvilasalo@gmail.com>
2017-08-07 20:41:16 +0200

    Handle animations by blueprint

---
+++ Source/ProjectMILA/EscapePodAnimInstance.cpp
@@ -0,0 +1,12 @@
+// All rights Neebula Games
+
+#include "ProjectMILA.h"
+#include "EscapePodAnimInstance.h"
+
+UEscapePodAnimInstance::UEscapePodAnimInstance(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer), bIsClosed(true)
+{
+}
+
+
+
