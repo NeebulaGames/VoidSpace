3cf7a7a5c595accc3df33b5f7550bd5cad534270
Ennio Casas Puglielli <theepsi@gmail.com>
2017-08-27 16:20:06 +0200

    Add Blink Light Logic

---
+++ Source/ProjectMILA/BlinkLights.cpp
@@ -0,0 +1,147 @@
+// All rights Neebula Games
+
+#include "ProjectMILA.h"
+#include "BlinkLights.h"
+#include "SpaceGameStateBase.h"
+#include "SpacestationManagementActor.h"
+
+
+// Sets default values
+ABlinkLights::ABlinkLights()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	USceneComponent* root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = root;
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> corridorUpperLight(TEXT("StaticMesh'/Game/Meshes/Props/Lights/PanelCorridorLight/PanelCorridorLight.PanelCorridorLight'"));
+
+	CorridorLeftLight = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LeftLight"));
+	CorridorLeftLight->SetStaticMesh(corridorUpperLight.Object);
+	CorridorLeftLight->SetupAttachment(RootComponent);
+
+	CorridorRightLight = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RightLight"));
+	CorridorRightLight->SetStaticMesh(corridorUpperLight.Object);
+	CorridorRightLight->SetupAttachment(RootComponent);
+
+	CorridorCentralLight = CreateDefaultSubobject<UPointLightComponent>(TEXT("CentralLight"));
+	CorridorCentralLight->SetIntensity(1100.f);
+	CorridorCentralLight->SetAttenuationRadius(500.f);
+	CorridorCentralLight->SetLightColor(FLinearColor(255.f,255.f,255.f));
+
+}
+
+// Called when the game starts or when spawned
+void ABlinkLights::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StationManager = ASpaceGameStateBase::Instance(GetWorld())->SpacestationManager;
+
+	LightIntensity = CorridorCentralLight->Intensity;
+	LightColor = CorridorCentralLight->GetLightColor();
+
+	int index = CorridorLeftLight->GetMaterialIndex("M_Light");
+	MaterialInstanceLeft = CorridorLeftLight->CreateAndSetMaterialInstanceDynamic(index);
+
+	index = CorridorRightLight->GetMaterialIndex("M_Light");
+	MaterialInstanceRight = CorridorRightLight->CreateAndSetMaterialInstanceDynamic(index);
+
+	ChangeLighting(StationManager->LightsState);
+	
+}
+
+// Called every frame
+void ABlinkLights::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	if (CurrentState != StationManager->LightsState && MaterialInstanceLeft && MaterialInstanceRight)
+	{
+		ChangeLighting(StationManager->LightsState);
+	}
+
+	// No blink when light off.
+	if (CurrentState != ELightState::LIGHT_OFF && Counter >= Frequency)
+	{
+		Counter = DeltaTime;
+		Frequency = FMath::RandRange(0.2f, 0.6f) + DeltaTime;
+		BlinkLights();
+	}
+	else
+	{
+		Counter += DeltaTime;
+	}
+	
+}
+
+void ABlinkLights::BlinkLights()
+{
+	if (CorridorCentralLight->Intensity != 0.f)
+	{
+		CorridorCentralLight->SetIntensity(0.f);
+	}
+	else
+	{
+		CorridorCentralLight->SetIntensity(LightIntensity);
+	}
+
+	float current;
+
+	MaterialInstanceLeft->GetScalarParameterValue("Color", current);
+
+	if (current != 0.f)
+	{
+		MaterialInstanceLeft->SetScalarParameterValue("Color", 0.f);
+		MaterialInstanceRight->SetScalarParameterValue("Color", 0.f);
+	}
+	else
+	{
+		float color;
+
+		switch (StationManager->LightsState)
+		{
+		case ELightState::LIGHT_EMERGENCY:
+			color = 2.f;
+			break;
+		case ELightState::LIGHT_ON:
+		default:
+			color = 1.f;
+			break;
+		}
+
+		MaterialInstanceLeft->SetScalarParameterValue("Color", color);
+		MaterialInstanceRight->SetScalarParameterValue("Color", color);
+	}	
+}
+
+void ABlinkLights::ChangeLighting(ELightState lightState)
+{
+	float color;
+
+	switch (lightState)
+	{
+	case ELightState::LIGHT_ON:
+		CorridorCentralLight->SetLightColor(LightColor);
+		CorridorCentralLight->SetIntensity(LightIntensity);
+		color = 1.f;
+		break;
+	case ELightState::LIGHT_OFF:
+		CorridorCentralLight->SetLightColor(LightColor);
+		CorridorCentralLight->SetIntensity(0.f);
+		color = 0.f;
+		break;
+	case ELightState::LIGHT_EMERGENCY:
+		CorridorCentralLight->SetLightColor(FLinearColor(FColor::Red));
+		CorridorCentralLight->SetIntensity(LightIntensity);
+		color = 2.f;
+		break;
+	}
+
+	MaterialInstanceLeft->SetScalarParameterValue("Color", color);
+	MaterialInstanceRight->SetScalarParameterValue("Color", color);
+
+	CurrentState = lightState;
+}
+
---
+++ Source/ProjectMILA/BlinkLights.h
@@ -0,0 +1,53 @@
+// All rights Neebula Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpaceStatics.h"
+#include "BlinkLights.generated.h"
+
+UCLASS()
+class PROJECTMILA_API ABlinkLights : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABlinkLights();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	void BlinkLights();
+	void ChangeLighting(ELightState lightState);
+
+	ELightState CurrentState;
+
+	float Counter = 0.f;
+	float Frequency = 0.f;
+
+	class ASpacestationManagementActor* StationManager;
+
+	UMaterialInstanceDynamic* MaterialInstanceLeft;
+	UMaterialInstanceDynamic* MaterialInstanceRight;
+
+	float LightIntensity;
+	FLinearColor LightColor;
+
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	UPointLightComponent* CorridorCentralLight;
+
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* CorridorLeftLight;
+
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* CorridorRightLight;
+};
